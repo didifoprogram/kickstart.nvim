917e6d3b61975253aa2732a71068162291f2221d
dfm <didifoprogram@gmail.com>
2024-11-24 11:44:10 -0300

    add lualine

--- init.lua
+++ init.lua
@@ -361,11 +361,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', fzf.buffers, { desc = '[ ] Find existing buffers' })
     end,
-  }  --         buffers = {
-  --           sort_mru = true,
-  --         },
-  --       },
-  --       extensions = {
+  }  --       extensions = {
   --         ['ui-select'] = {
   --           require('telescope.themes').get_dropdown(),
   --         },
@@ -415,7 +411,7 @@ require('lazy').setup({
   -- },
 
   -- LSP Plugins
-, -- { -- Fuzzy Finder (files, lsp, etc) --   'nvim-telescope/telescope.nvim', --   event = 'VimEnter', --   branch = '0.1.x', --   dependencies = { --     'nvim-lua/plenary.nvim', --     { -- If encountering errors, see telescope-fzf-native README for installation instructions --       'nvim-telescope/telescope-fzf-native.nvim', -- -- --       -- `build` is used to run some command when the plugin is installed/updated. --       -- This is only run then, not every time Neovim starts up. --       build = 'make', -- --       -- `cond` is a condition used to determine whether this plugin should be --       -- installed and loaded. --       cond = function() --         return vim.fn.executable 'make' == 1 --       end, --     }, --     { 'nvim-telescope/telescope-ui-select.nvim' }, -- --     -- Useful for getting pretty icons, but requires a Nerd Font. --     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font }, --   }, --   config = function() --     -- Telescope is a fuzzy finder that comes with a lot of different things that --     -- it can fuzzy find! It's more than just a "file finder", it can search -- many different aspects of Neovim, your workspace, LSP, and more! --     -- --     -- The easiest way to use Telescope, is to start by doing something like: --     --  :Telescope help_tags --     -- --     -- After running this command, a window will open up and you're able to --     -- type in the prompt window. You'll see a list of `help_tags` options and --     -- a corresponding preview of the help. --     -- --     -- Two important keymaps to use while in Telescope are: --     --  - Insert mode: <c-/> --     --  - Normal mode: ? --     -- --     -- This opens a window that shows you all of the keymaps for the current --     -- Telescope picker. This is really useful to discover what Telescope can --     -- do as well as how to actually do it! -- --     -- [[ Configure Telescope ]] --     -- See `:help telescope` and `:help telescope.setup()` --     require('telescope').setup { --       -- You can put your default mappings / updates / etc. in here --       --  All the info you're looking for is in `:help telescope.setup()` --       -- --       -- defaults = { --       --   mappings = { --       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' }, --       --   }, --       -- }, --       pickers = { --         find_files = { --           sort_mru = true, --         },
+, -- { -- Fuzzy Finder (files, lsp, etc) --   'nvim-telescope/telescope.nvim', --   event = 'VimEnter', --   branch = '0.1.x', --   dependencies = { --     'nvim-lua/plenary.nvim', --     { -- If encountering errors, see telescope-fzf-native README for installation instructions --       'nvim-telescope/telescope-fzf-native.nvim', -- -- --       -- `build` is used to run some command when the plugin is installed/updated. --       -- This is only run then, not every time Neovim starts up. --       build = 'make', -- --       -- `cond` is a condition used to determine whether this plugin should be --       -- installed and loaded. --       cond = function() --         return vim.fn.executable 'make' == 1 --       end, --     }, --     { 'nvim-telescope/telescope-ui-select.nvim' }, -- --     -- Useful for getting pretty icons, but requires a Nerd Font. --     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font }, --   }, --   config = function() --     -- Telescope is a fuzzy finder that comes with a lot of different things that --     -- it can fuzzy find! It's more than just a "file finder", it can search -- many different aspects of Neovim, your workspace, LSP, and more! --     -- --     -- The easiest way to use Telescope, is to start by doing something like: --     --  :Telescope help_tags --     -- --     -- After running this command, a window will open up and you're able to --     -- type in the prompt window. You'll see a list of `help_tags` options and --     -- a corresponding preview of the help. --     -- --     -- Two important keymaps to use while in Telescope are: --     --  - Insert mode: <c-/> --     --  - Normal mode: ? --     -- --     -- This opens a window that shows you all of the keymaps for the current --     -- Telescope picker. This is really useful to discover what Telescope can --     -- do as well as how to actually do it! -- --     -- [[ Configure Telescope ]] --     -- See `:help telescope` and `:help telescope.setup()` --     require('telescope').setup { --       -- You can put your default mappings / updates / etc. in here --       --  All the info you're looking for is in `:help telescope.setup()` --       -- --       -- defaults = { --       --   mappings = { --       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' }, --       --   }, --       -- }, --       pickers = { --         find_files = { --           sort_mru = true, --         }, --         buffers = { --           sort_mru = true, --         }, --       },
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
@@ -865,17 +861,17 @@ require('lazy').setup({
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
+      -- local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- ---@diagnostic disable-next-line: duplicate-set-field
+      -- statusline.section_location = function()
+      --   return '%2l:%-2v'
+      -- end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -19,4 +19,5 @@ return {
   require 'custom.plugins.oil',
   require 'custom.plugins.rustacean',
   require 'custom.plugins.crates',
+  require 'custom.plugins.lualine',
 }
